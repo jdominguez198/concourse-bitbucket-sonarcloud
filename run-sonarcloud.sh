9955e318fb4f97439a13cd8c769e6a7a8b2d420a
Jesus Dominguez <jdominguez@onestic.com>
2020-03-19 12:46:08 +0100

    Fix variable substitution; Add verbosity

--- run-sonarcloud.sh
+++ run-sonarcloud.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 : "${INPUT_FOLDER:?}"
-: "${$REPOSITORY_EXCLUSIONS:?}"
-: "${$REPOSITORY_SOURCES:?}"
-: "${$SONAR_PROJECT_KEY:?}"
-: "${$SONAR_PROJECT_NAME:?}"
-: "${$SONAR_PROJECT_ORGANIZATION:?}"
-: "${$SONAR_TOKEN:?}"
+: "${REPOSITORY_EXCLUSIONS:?}"
+: "${REPOSITORY_SOURCES:?}"
+: "${SONAR_PROJECT_KEY:?}"
+: "${SONAR_PROJECT_NAME:?}"
+: "${SONAR_PROJECT_ORGANIZATION:?}"
+: "${SONAR_TOKEN:?}"
 
 echo ">>>> Getting Pull Request info if exists..."
 if [[ -f "$INPUT_FOLDER/pull-request-info" ]]; then
+  echo ">>>> Running SonarCloud tests for Pull Request..."
   export PR_ID=$(jq -r '.id' "$INPUT_FOLDER/pull-request-info")
   export PR_BRANCH=$(jq -r '.feature_branch' "$INPUT_FOLDER/pull-request-info")
   export PR_BASE=$(jq -r '.upstream_branch' "$INPUT_FOLDER/pull-request-info")
@@ -29,7 +30,8 @@ if [[ -f "$INPUT_FOLDER/pull-request-info" ]]; then
     -Dsonar.login=$SONAR_TOKEN \
     -Dsonar.exclusions=$REPOSITORY_EXCLUSIONS
 else
-    docker run -ti -v $(pwd)/$INPUT_FOLDER:/usr/src newtmitch/sonar-scanner:alpine \
+  echo ">>>> Running SonarCloud tests..."
+  docker run -ti -v $(pwd)/$INPUT_FOLDER:/usr/src newtmitch/sonar-scanner:alpine \
     -Dsonar.projectBaseDir=/usr/src \
     -Dsonar.projectKey=$SONAR_PROJECT_KEY \
     -Dsonar.projectName=$SONAR_PROJECT_NAME \

723f3f6079380c2bae5787318b21b6ec90740377
Jesus Dominguez <jdominguez@onestic.com>
2020-03-19 12:29:05 +0100

    Add files based on concourse-dcind

---
+++ run-sonarcloud.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+: "${INPUT_FOLDER:?}"
+: "${$REPOSITORY_EXCLUSIONS:?}"
+: "${$REPOSITORY_SOURCES:?}"
+: "${$SONAR_PROJECT_KEY:?}"
+: "${$SONAR_PROJECT_NAME:?}"
+: "${$SONAR_PROJECT_ORGANIZATION:?}"
+: "${$SONAR_TOKEN:?}"
+
+echo ">>>> Getting Pull Request info if exists..."
+if [[ -f "$INPUT_FOLDER/pull-request-info" ]]; then
+  export PR_ID=$(jq -r '.id' "$INPUT_FOLDER/pull-request-info")
+  export PR_BRANCH=$(jq -r '.feature_branch' "$INPUT_FOLDER/pull-request-info")
+  export PR_BASE=$(jq -r '.upstream_branch' "$INPUT_FOLDER/pull-request-info")
+  docker run -ti -v $(pwd)/$INPUT_FOLDER:/usr/src newtmitch/sonar-scanner:alpine \
+    -Dsonar.projectBaseDir=/usr/src \
+    -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+    -Dsonar.projectName=$SONAR_PROJECT_NAME \
+    -Dsonar.organization=$SONAR_PROJECT_ORGANIZATION \
+    -Dsonar.pullrequest.provider=bitbucketcloud \
+    -Dsonar.pullrequest.bitbucketcloud.owner=$REPOSITORY_OWNER \
+    -Dsonar.pullrequest.bitbucketcloud.repository=$REPOSITORY_NAME \
+    -Dsonar.pullrequest.key=$PR_ID \
+    -Dsonar.pullrequest.branch=$PR_BRANCH \
+    -Dsonar.pullrequest.base=$PR_BASE \
+    -Dsonar.sources=$REPOSITORY_SOURCES \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.exclusions=$REPOSITORY_EXCLUSIONS
+else
+    docker run -ti -v $(pwd)/$INPUT_FOLDER:/usr/src newtmitch/sonar-scanner:alpine \
+    -Dsonar.projectBaseDir=/usr/src \
+    -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+    -Dsonar.projectName=$SONAR_PROJECT_NAME \
+    -Dsonar.organization=$SONAR_PROJECT_ORGANIZATION \
+    -Dsonar.sources=$REPOSITORY_SOURCES \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.exclusions=$REPOSITORY_EXCLUSIONS
+fi
+
+

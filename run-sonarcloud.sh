f04b784c0a01c3f0a110ac82f0b3f7cb9d04a904
Jesus Dominguez <jdominguez@onestic.com>
2020-03-31 17:47:19 +0200

    Test checkout branches

--- run-sonarcloud.sh
+++ run-sonarcloud.sh
@@ -9,6 +9,24 @@
 : "${SONAR_PROJECT_ORGANIZATION:?}"
 : "${SONAR_TOKEN:?}"
 
+call_sonarcloud_docker_run () {
+  docker run -ti -v $(pwd)/$INPUT_FOLDER:/usr/src newtmitch/sonar-scanner:alpine \
+    -Dsonar.projectBaseDir=/usr/src \
+    -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+    -Dsonar.projectName=$SONAR_PROJECT_NAME \
+    -Dsonar.organization=$SONAR_PROJECT_ORGANIZATION \
+    -Dsonar.pullrequest.provider=bitbucketcloud \
+    -Dsonar.pullrequest.bitbucketcloud.owner=$REPOSITORY_OWNER \
+    -Dsonar.pullrequest.bitbucketcloud.repository=$REPOSITORY_NAME \
+    -Dsonar.pullrequest.key=$PR_ID \
+    -Dsonar.pullrequest.branch=$PR_BRANCH \
+    -Dsonar.pullrequest.base=$PR_BASE \
+    -Dsonar.sources=$REPOSITORY_SOURCES \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.exclusions=$REPOSITORY_EXCLUSIONS
+}
+
 echo ">>>> Loading repository credentials..."
 mkdir -p /root/.ssh && \
     chmod 0700 /root/.ssh && \
@@ -16,21 +34,21 @@ mkdir -p /root/.ssh && \
     echo "$REPOSITORY_SSH_KEY" > /root/.ssh/id_rsa && \
     chmod 600 /root/.ssh/id_rsa
 
-echo ">>>> Fetching files from git repository..."
-SOURCE_DIR=$(pwd)
-cd $INPUT_FOLDER
-git checkout -B $PR_BASE origin/$PR_BASE
-git fetch --all
-git branch -l
-git checkout $PR_BRANCH
-cd $SOURCE_DIR
-
 echo ">>>> Getting Pull Request info if exists..."
 if [[ -f "$INPUT_FOLDER/pull-request-info" ]]; then
-  echo ">>>> Running SonarCloud tests for Pull Request..."
+  echo ">>>> Fetching data from Pull Request"
   export PR_ID=$(jq -r '.id' "$INPUT_FOLDER/pull-request-info")
   export PR_BRANCH=$(jq -r '.feature_branch' "$INPUT_FOLDER/pull-request-info")
   export PR_BASE=$(jq -r '.upstream_branch' "$INPUT_FOLDER/pull-request-info")
+  echo ">>>> Fetching files from git repository..."
+  SOURCE_DIR=$(pwd)
+  cd $INPUT_FOLDER
+  git checkout -B $PR_BASE origin/$PR_BASE
+  git fetch --all
+  git branch -l
+  git checkout $PR_BRANCH
+  cd $SOURCE_DIR
+  echo ">>>> Running SonarCloud tests for Pull Request..."
   docker run -ti -v $(pwd)/$INPUT_FOLDER:/usr/src newtmitch/sonar-scanner:alpine \
     -Dsonar.projectBaseDir=/usr/src \
     -Dsonar.projectKey=$SONAR_PROJECT_KEY \
